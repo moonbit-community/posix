bca974904adb1fc7e347cf27a33a831c017b8368
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-05-02 07:19:08 +0800

    refactor: remove stub directory use stub.c instead

--- stub/src/gnu/unistd_conf.c
+++
@@ -1,6 +0,0 @@
-#include <moonbit_posix_types.h>
-#include <unistd.h>
-
-// FFI namespace moonbit_posix_
-int32_t moonbit_posix__CS_GNU_LIBC_VERSION() { return _CS_GNU_LIBC_VERSION; }
-int32_t moonbit_posix__CS_GNU_LIBPTHREAD_VERSION() { return _CS_GNU_LIBPTHREAD_VERSION; }
